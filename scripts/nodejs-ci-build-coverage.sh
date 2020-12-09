7693b2e9494a8f0adda8c806ac8892c71a6491c8
Mingtong Deng <mingtong@amazon.com>
2020-12-09 14:49:23 +0800

    Add nodejs UT and coverage

--- scripts/nodejs-ci-build-coverage.sh
+++ scripts/nodejs-ci-build-coverage.sh
@@ -3,20 +3,29 @@
 JQ_EXEC=`which jq`
 
 FILE_PATH=edge/nodejs/nodejs.json
-
 new_app_code_uri=$(cat $FILE_PATH | ${JQ_EXEC} .edge.new[].codeuri | sed 's/\"//g')
 update_app_code_uri=$(cat $FILE_PATH | ${JQ_EXEC} .edge.update[].codeuri | sed 's/\"//g')
 
 # build and code coverage
-npm i nyc --save-dev
-npm install --save-dev mocha 
-npm install --save-dev chai 
+current_path=$(pwd)
+echo "@@@@@@@@@@@@@@@@@ current path" $current_path
+
 for var in ${new_app_code_uri[@]}; do
-	npm --prefix edge/nodejs/$var run test
+	cd $current_path/edge/nodejs/$var
+	echo "working path" $(pwd)
+	npm i -D nyc
+	npm install --save-dev mocha
+	npm install --save-dev chai 	
+	npm test
 done
 
 for var in ${update_app_code_uri[@]}; do
-	npm --prefix edge/nodejs/$var run test
+	cd $current_path/edge/nodejs/$var
+	echo "working path" $(pwd)
+	npm i -D nyc
+	npm install --save-dev mocha
+	npm install --save-dev chai
+	npm test
 done
 
 
