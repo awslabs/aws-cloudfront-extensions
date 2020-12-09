3da79d7f7f5d307268dd1e2fcb6e892e8b996fc8
Mingtong Deng <mingtong@amazon.com>
2020-12-09 21:38:24 +0800

    test sam packaging and publishing action

--- scripts/sam-publish.sh
+++ scripts/sam-publish.sh
@@ -4,9 +4,9 @@
 JQ_EXEC=`which jq`
 
 FILE_PATH=edge/publish-apps.json
+export SAM_CLI_TELEMETRY=0
 
 app_path_list=$(cat $FILE_PATH | ${JQ_EXEC} .edge.publish_app_path[] | sed 's/\"//g')
-
 
 for var in ${app_path_list[@]}; do
 	echo $var
