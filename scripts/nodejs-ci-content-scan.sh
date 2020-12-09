d44a32f21894a7f12303eacc7e9d20ae8473db01
Mingtong Deng <mingtong@amazon.com>
2020-12-09 12:53:46 +0800

    update test

--- scripts/nodejs-ci-content-scan.sh
+++ scripts/nodejs-ci-content-scan.sh
@@ -1,24 +1,24 @@
 #! /bin/bash
  
-# JQ_EXEC=`which jq`
+JQ_EXEC=`which jq`
 
-# FILE_PATH=edge/nodejs/nodejs.json
+FILE_PATH=edge/nodejs/nodejs.json
 
-# new_app_list=$(cat $FILE_PATH | ${JQ_EXEC} .edge.new[].appname | sed 's/\"//g')
-# update_app_list=$(cat $FILE_PATH | ${JQ_EXEC} .edge.update[].appname | sed 's/\"//g')
+new_app_list=$(cat $FILE_PATH | ${JQ_EXEC} .edge.new[].appname | sed 's/\"//g')
+update_app_list=$(cat $FILE_PATH | ${JQ_EXEC} .edge.update[].appname | sed 's/\"//g')
 
 # scan commit content
 aws s3 cp s3://aws-solutions-build-assets/viperlight-scanner/viperlight.zip .
 
 unzip -q viperlight.zip -d ./viperlight
 
-# for var in ${new_app_list[@]}; do
-# 	./viperlight/bin/viperlight-scan -t edge/nodejs/$var
-# done
+for var in ${new_app_list[@]}; do
+	./viperlight/bin/viperlight-scan -t edge/nodejs/$var
+done
 
-# for var in ${update_app_list[@]}; do
-# 	./viperlight/bin/viperlight-scan -t edge/nodejs/$var
-# done
+for var in ${update_app_list[@]}; do
+	./viperlight/bin/viperlight-scan -t edge/nodejs/$var
+done
 
 
 
