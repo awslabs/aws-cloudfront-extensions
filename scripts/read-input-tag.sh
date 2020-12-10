741aa8a0812bbd43b141aca5e9f974953082864d
Mingtong Deng <mingtong@amazon.com>
2020-12-10 15:57:45 +0800

    add tag before publish apps

---
+++ scripts/read-input-tag.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+ 
+JQ_EXEC=`which jq`
+
+FILE_PATH=edge/publish-apps.json
+
+tag_value=$(cat $FILE_PATH | ${JQ_EXEC} .edge.tag | sed 's/\"//g')
+
+echo $tag_value
+
+
+
+
+
+
+
+
+
+
+
