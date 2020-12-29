3766b8a383c8b73ef5c8c78ed056567cc3f37af7
dengmingtong <dengmingtong@hotmail.com>
2020-12-29 21:47:37 +0800

    1, change authentication dependencies. 2, change sam-package.sh

--- scripts/sam-package.sh
+++ scripts/sam-package.sh
@@ -5,53 +5,23 @@ JQ_EXEC=`which jq`
 FILE_PATH=edge/publish-apps.json
 export SAM_CLI_TELEMETRY=0
 
-FILTER_PACKAGE_ARRAY=('aws-sdk' 'axios' 'nyc')
-PACKAGE_FILE_PATH=package.json
-
 app_path_list=$(cat $FILE_PATH | ${JQ_EXEC} .edge.publish_app_path[] | sed 's/\"//g')
 
-for var in ${app_path_list[@]}; do
-	echo $var
-	cd edge/$var
+for app_path in ${app_path_list[@]}; do
+	export codeUri="$(cut -d'/' -f2 <<<"$app_path")"
+	echo $app_path
+	echo $codeUri
+	cd edge/$app_path/$codeUri
 	echo $(pwd)
-	
-	codePaths=$(find . -name "package.json")
-	for codePath in ${codePaths[@]}; do
-		IFS='/' read -ra codePackage <<< "$codePath"
-		echo ${codePackage[1]}
-		cd ${codePackage[1]}
-		npm install
-		dependencies=$(cat $PACKAGE_FILE_PATH | ${JQ_EXEC} '.dependencies | keys[]' | sed 's/\"//g')
-		tmpDir='dependencies-tmp'
-		mkdir $tmpDir
-		for var in ${dependencies[@]}; do
-			echo $var
-			if [[ " ${FILTER_PACKAGE_ARRAY[*]} " == *" $var "* ]]; 
-			then
-				echo "ignore"
-			else
-				echo "backup dependency package"
-				mv node_modules/$var ./$tmpDir
-			fi
-		done
 
-		cd node_modules
-		rm -rf $(find . -name "*" ! -name ".*")
+	npm install --production
 
-		cd ../
+	zip -r $codeUri.zip *
+	aws s3 cp $codeUri.zip s3://aws-cloudfront-extension-lambda-edge/edge/$codeUri/$codeUri.zip --acl public-read
 
-		cp -rf $tmpDir/* node_modules/
-
-		rm -rf $tmpDir
-
-		zip -r ${codePackage[1]}.zip *
-		aws s3 cp ${codePackage[1]}.zip s3://aws-cloudfront-extension-lambda-edge/edge/${codePackage[1]}/${codePackage[1]}.zip --acl public-read
-		rm -f ${codePackage[1]}.zip
-
-	done
-	cd ../../../	
+	cd ../../../../
 	echo $(pwd)
-	sam package -t edge/$var/template.yaml --output-template-file edge/$var/packaged.yaml --s3-bucket cloudfront-extensions-package --region us-east-1	
+	sam package -t edge/$app_path/template.yaml --output-template-file edge/$app_path/packaged.yaml --s3-bucket cloudfront-extensions-package --region us-east-1	
 done
 
 
