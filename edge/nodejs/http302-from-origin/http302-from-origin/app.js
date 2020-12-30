c820d9a2751f3e099142190047968cf93385e3a5
cc4i <chuancc@gmail.com>
2020-12-30 20:29:19 +0800

    update http302-from-origin

--- edge/nodejs/http302-from-origin/http302-from-origin/app.js
+++ edge/nodejs/http302-from-origin/http302-from-origin/app.js
@@ -2,14 +2,59 @@
 
 const { http, https } = require('follow-redirects');
 
+
 exports.handler = (event, context, callback) => {
     const response = event.Records[0].cf.response;
-    
-    if (response.status == 200) {
-        response.headers['location']
-    }
-    
+    getData(function(data){
+        const newResponse = {
+            status: '200',
+            statusDescription: 'OK',
+            headers: {
+                'cache-control': [{
+                    key: 'Cache-Control',
+                    value: 'max-age=100'
+                }],
+                'content-type': [{
+                    key: 'Content-Type',
+                    value: 'text/html'
+                }]
+            },
+            body: data,
+        };
+        console.log(newResponse)
+        callback(null, newResponse);
+    }, response);
 
-
-    callback(null, response);
+    
 };
+
+
+function getData(callbackData, response) {
+    var content=[], url, isHttps;
+
+    var callback = function(res) {
+        res.on('data', chunk => {
+            content.push(chunk);
+        });
+        res.on('end', function() {
+            callbackData(content);
+        });
+    }
+
+    if (response.status == 302 && response.headers['location']!=0) {
+        url = response.headers['location'][0].value;
+        isHttps = (url.substring(0,5)=="https");
+
+        if (isHttps) {
+            https.get(url, callback).on('error', err => {
+                console.error(err);
+            });
+        } else {
+            http.get(url, callback).on('error', err => {
+                console.error(err);
+            });
+
+        }
+
+    }
+}
