de76779f539627e94ff589f812ce266435b2b398
Ning <lvning@amazon.com>
2021-01-11 18:30:05 +0800

    Reformat and read from file to check issue compliance as the issue content contains multiple lines

--- scripts/python/check_issue.py
+++ scripts/python/check_issue.py
@@ -4,7 +4,7 @@ from optparse import OptionParser
 
 log = logging.getLogger('aws-cloudfront-extension.check_issue')
 log_formatter = logging.Formatter(
-  '[%(asctime)s %(name)s][%(levelname)s] %(message)s')
+    '[%(asctime)s %(name)s][%(levelname)s] %(message)s')
 log_stream_handler = logging.StreamHandler(sys.stdout)
 log_stream_handler.setFormatter(log_formatter)
 log.addHandler(log_stream_handler)
@@ -16,44 +16,57 @@ ACTUAL_RESULT = "### What actually happened?"
 
 
 def parse_opt():
-  parser = OptionParser(
-      usage="Usage: python check_issue.py [options]\n\t Check compliance for the content")
-  parser.add_option("-b", "--body",
-                    dest="body",
-                    help="The issue content body")
-  option, args = parser.parse_args()
-  return parser, option, args
+    parser = OptionParser(
+        usage="Usage: python check_issue.py [options]\n\t Check compliance for the content")
+    parser.add_option("-b", "--body",
+                      dest="body",
+                      help="The issue content body")
+    option, args = parser.parse_args()
+    return parser, option, args
 
 
 def check_issue():
-  parser, option, args = parse_opt()
+    parser, option, args = parse_opt()
 
-  if not option.body:
-    log.error('Missing arguments: -b or --body')
-    parser.print_help()
-    sys.exit(1)
+    if not option.body:
+        log.error('Missing arguments: -b or --body')
+        parser.print_help()
+        sys.exit(1)
 
-  issue_content = option.body
-  log.info('Issue content: ' + issue_content)
-  index_repro = issue_content.find(REPRO_STEP)
-  index_expected = issue_content.find(EXPECTED_RESULT)
-  index_actual = issue_content.find(ACTUAL_RESULT)
+    issue_content = get_issue_from_file(option.body)
+    log.info('Issue content: ' + issue_content)
+    index_repro = issue_content.find(REPRO_STEP)
+    index_expected = issue_content.find(EXPECTED_RESULT)
+    index_actual = issue_content.find(ACTUAL_RESULT)
 
-  if index_repro == -1 or index_expected == -1 or index_actual == -1:
-    log.error('Please fill in the information by using the template')
-    sys.exit(1)
+    if index_repro == -1 or index_expected == -1 or index_actual == -1:
+        log.error('Please fill in the information by using the template')
+        sys.exit(1)
 
-  repro_content = issue_content[index_repro + len(REPRO_STEP), index_expected]
-  expected_content = issue_content[
-    index_expected + len(EXPECTED_RESULT), index_actual]
+    repro_content = issue_content[index_repro + len(REPRO_STEP): index_expected]
+    expected_content = issue_content[
+        index_expected + len(EXPECTED_RESULT): index_actual]
 
-  log.info('Reproduce steps: ' + repro_content)
-  log.info('Expected result: ' + expected_content)
-  if len(repro_content.strip()) == 0 or len(expected_content.strip()) == 0:
-    log.error(
-      'Empty reproduce steps or expected result, please fill in these fields')
-    sys.exit(1)
+    log.info('Reproduce steps: ' + repro_content)
+    log.info('Expected result: ' + expected_content)
+    if len(repro_content.strip()) == 0 or len(expected_content.strip()) == 0:
+        log.error(
+            'Empty reproduce steps or expected result, please fill in these fields')
+        sys.exit(1)
+
+    log.info('Check issue compliance succeed')
+
+
+def get_issue_from_file(file_name):
+    f = open(file_name, "r")
+    process_lines = f.readlines()
+    file_content = ''
+    for line in process_lines:
+        file_content += line
+
+    f.close()
+    return file_content
 
 
 if __name__ == '__main__':
-  check_issue()
+    check_issue()
