8134a137f0ab5fd4108faba1eb8175ccb86328df
ming gu <guming3d@gmail.com>
2021-01-22 17:50:19 +0800

    adding multiple retry lambda edge (#73)
    *Issue #, if available:*
    
    *Description of changes:*
    Adding lambda edge for  this requirement: 
    客户源站域名有多个IP，当第一个IP访问失败后，需要轮询其他直到回源成功，或轮询完所有IP都失败
    
    *How Has This Been Tested:*
    Unit test and my CloudFront account in us-east-1
    
    *[x] My testing has passed*
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

---
+++ edge/nodejs/multiple-origin-IP-retry/multiple-origin-IP-retry/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  target: 'node',
+  node: {
+    __dirname: false
+  },
+  entry: {
+    '/bundle': path.resolve(__dirname, './app.js'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+        options: {
+          minimize: true,
+        },
+      },
+    ]
+  },
+  resolve: {
+    extensions: [ '.ts', '.js' ]
+  },
+  output: {
+    path: path.resolve(__dirname),
+    filename: '[name].js',
+    libraryTarget: 'commonjs',
+  },
+  externals: [
+    /^aws-sdk/ // Don't include the aws-sdk in bundles as it is already present in the Lambda runtime
+  ],
+  performance: {
+    hints: 'error',
+    maxAssetSize: 1048576, // Max size of deployment bundle in Lambda@Edge Viewer Request
+    maxEntrypointSize: 1048576, // Max size of deployment bundle in Lambda@Edge Viewer Request
+  },
+  optimization: {
+    minimizer: [new TerserPlugin({
+      cache: true,
+      parallel: true,
+      extractComments: true,
+    })],
+  },
+}
