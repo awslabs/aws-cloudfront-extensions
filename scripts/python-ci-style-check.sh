624023c198454970ea54be58af1a65e7e343b8fc
Mike Yan <yiyanz@amazon.com>
2021-06-03 10:24:49 +0800

    Update python-ci-style-check.sh

--- scripts/python-ci-style-check.sh
+++ scripts/python-ci-style-check.sh
@@ -8,5 +8,7 @@
 # update_app_code_uri=$(cat $FILE_PATH | ${JQ_EXEC} .edge.update[].codeuri | sed 's/\"//g')
 
 #check code style
+echo edge/$labelName
 export PYTHONPATH="${PWD}/edge/${labelName}/${labelName#*/}"
+echo $PYTHONPATH
 pylint --rcfile .pylintrc edge/$labelName/
