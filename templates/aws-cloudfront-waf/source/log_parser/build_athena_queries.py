066056e64e30f2cf9cbe025847c5876ccd9367d6
Ning <lvning@amazon.com>
2021-11-10 11:10:02 +0800

    fix: add CODEOWNERS file

--- templates/aws-cloudfront-waf/source/log_parser/build_athena_queries.py
+++ templates/aws-cloudfront-waf/source/log_parser/build_athena_queries.py
@@ -17,7 +17,7 @@ import datetime
 
 
 def build_athena_query_for_app_access_logs(
-    log, log_type, database_name, table_name, end_timestamp,
+    log, database_name, table_name, end_timestamp,
         waf_block_period, error_threshold):
     """
     This function dynamically builds athena query
@@ -58,12 +58,9 @@ def build_athena_query_for_app_access_logs(
         "[build_athena_query_for_app_access_logs]  \
             Build query")
     # --------------------------------------------------
-    if log_type == 'CLOUDFRONT':
-        query_string = build_athena_query_part_one_for_cloudfront_logs(
-            log, database_name, table_name)
-    else:  # ALB logs
-        query_string = build_athena_query_part_one_for_alb_logs(
-            log, database_name, table_name)
+
+    query_string = build_athena_query_part_one_for_cloudfront_logs(
+        log, database_name, table_name)
     query_string = query_string +  \
         build_athena_query_part_two_for_partition(
             log, start_timestamp, end_timestamp)
@@ -77,67 +74,6 @@ def build_athena_query_for_app_access_logs(
 
     log.info(
         '[build_athena_query_for_app_access_logs] End')
-
-    return query_string
-
-
-def build_athena_query_for_waf_logs(
-    log, database_name, table_name, end_timestamp,
-        waf_block_period, request_threshold):
-    """
-    This function dynamically builds athena query
-    for cloudfront logs by adding partition values:
-    year, month, day, hour. It splits query into three
-    parts, builds them one by one and then concatenate
-    them together into one final query.
-
-    Args:
-        log: logging object
-        database_name: string. The Athena/Glue database name
-        table_name: string. The Athena/Glue table name
-        end_timestamp: datetime. The end time stamp of the logs being scanned
-        waf_block_period: int. The period (in minutes) to block applicable IP addresses
-        request_threshold: int. The maximum acceptable bad requests per minute per IP address
-
-    Returns:
-        Athena query string
-    """
-    log.info(
-        '[build_athena_query_for_waf_logs] Start')
-
-    # ------------------------------------------------
-    log.debug(
-        "[build_athena_query_for_waf_logs] \
-            Get start and end time stamps")
-    # ------------------------------------------------
-    query_string = ""
-    start_timestamp = end_timestamp - \
-        datetime.timedelta(seconds=60*waf_block_period)
-    log.info(
-        "[build_athena_query_for_waf_logs]  \
-            start time: %s; end time: %s"
-            %(start_timestamp, end_timestamp))
-
-    # -------------------------------------------------
-    log.debug(
-        "[build_athena_query_for_waf_logs]  \
-            Build query")
-    # --------------------------------------------------
-    query_string = build_athena_query_part_one_for_waf_logs(
-        log, database_name, table_name)
-    query_string = query_string +  \
-        build_athena_query_part_two_for_partition(
-            log, start_timestamp, end_timestamp)
-    query_string = query_string +  \
-        build_athena_query_part_three_for_waf_logs(
-            log, request_threshold, start_timestamp, end_timestamp)
-
-    log.info(
-        "[build_athena_query_for_waf_logs]  \
-            Query string:\n %s"%query_string)
-
-    log.info(
-        '[build_athena_query_for_waf_logs] End')
 
     return query_string
 
@@ -170,69 +106,6 @@ def build_athena_query_part_one_for_cloudfront_logs(
                         + database_name + "." + table_name
     log.debug(
         "[build_athena_query_part_one_for_cloudfront_logs]  \
-         Query string part One:\n %s"%query_string)
-    return query_string
-
-
-def build_athena_query_part_one_for_alb_logs(
-        log, database_name, table_name):
-    """
-    This function dynamically builds the first part
-    of the athena query.
-
-    Args:
-        log: logging object
-        database_name: string. The Athena/Glue database name
-        table_name: string. The Athena/Glue table name
-
-    Returns:
-        Athena query string
-    """
-    query_string = "SELECT\n" \
-                        "\tclient_ip,\n" \
-                        "\tMAX_BY(counter, counter) as max_counter_per_min\n"  \
-                   " FROM (\n"  \
-                      "\tWITH logs_with_concat_data AS (\n"  \
-                        "\t\tSELECT\n"  \
-                          "\t\t\tclient_ip,\n"  \
-                          "\t\t\ttarget_status_code AS status,\n"  \
-                          "\t\t\tparse_datetime(time, 'yyyy-MM-dd''T''HH:mm:ss.SSSSSS''Z') AS datetime\n"  \
-                        "\t\tFROM\n" \
-                        + "\t\t\t" \
-                        + database_name + "." + table_name
-    log.debug(
-        "[build_athena_query_part_one_for_alb_logs]  \
-         Query string part One:\n %s"%query_string)
-    return query_string
-
-
-def build_athena_query_part_one_for_waf_logs(
-        log, database_name, table_name):
-    """
-    This function dynamically builds the first part
-    of the athena query.
-
-    Args:
-        log: logging object
-        database_name: string. The Athena/Glue database name
-        table_name: string. The Athena/Glue table name
-
-    Returns:
-        Athena query string
-    """
-    query_string = "SELECT\n" \
-                        "\tclient_ip,\n" \
-                        "\tMAX_BY(counter, counter) as max_counter_per_min\n"  \
-                   " FROM (\n"  \
-                      "\tWITH logs_with_concat_data AS (\n"  \
-                        "\t\tSELECT\n"  \
-                          "\t\t\thttprequest.clientip as client_ip,\n"  \
-                          "\t\t\tfrom_unixtime(timestamp/1000) as datetime\n"  \
-                        "\t\tFROM\n" \
-                        + "\t\t\t" \
-                        + database_name + "." + table_name
-    log.debug(
-        "[build_athena_query_part_one_for_waf_logs]  \
          Query string part One:\n %s"%query_string)
     return query_string
 
@@ -350,44 +223,3 @@ def build_athena_query_part_three_for_app_access_logs(
         Query string part Three:\n %s"%query_string)
     return query_string
 
-
-def build_athena_query_part_three_for_waf_logs(
-        log, request_threshold, start_timestamp, end_timestamp):
-    """
-    This function dynamically builds the third part
-    of the athena query.
-
-    Args:
-        log: logging object
-        error_threshold: int. The maximum acceptable bad requests per minute per IP address
-        start_timestamp: datetime. The start time stamp of the logs being scanned
-        end_timestamp: datetime. The end time stamp of the logs being scanned
-
-    Returns:
-        Athena query string
-    """
-    request_threshold_calculated = request_threshold / 5
-    query_string = "\n\t)\n"  \
-                   "\tSELECT\n"  \
-                   "\t\tclient_ip,\n"  \
-                   "\t\tCOUNT(*) as counter\n"  \
-                   "\tFROM\n"  \
-                   "\t\tlogs_with_concat_data\n"  \
-                   "\tWHERE\n"  \
-                   "\t\tdatetime > TIMESTAMP "  \
-                   + "'" + str(start_timestamp)[0:19] + "'"\
-                   "\n\tGROUP BY\n"  \
-                   "\t\tclient_ip,\n"  \
-                   "\t\tdate_trunc('minute', datetime)\n"  \
-                   "\tHAVING\n"  \
-                   "\t\tCOUNT(*) >= "  \
-                   + str(request_threshold_calculated) + \
-                   "\n) GROUP BY\n"  \
-                   "\tclient_ip\n"  \
-                   "ORDER BY\n" \
-                   "\tmax_counter_per_min DESC\n" \
-                   "LIMIT 10000;"
-    log.debug(
-        "[build_athena_query_part_three_for_waf_logs]  \
-        Query string part Three:\n %s"%query_string)
-    return query_string
