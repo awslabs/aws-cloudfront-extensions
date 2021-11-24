3ac4d5fbc31f9fd7081d68b8e7e68ff2191cf3be
George Gu <minggu@amazon.com>
2021-11-24 16:26:01 +0800

    update the build script

--- templates/aws-cloudfront-monitoring/deployment/helper.py
+++ templates/aws-cloudfront-monitoring/deployment/helper.py
@@ -80,7 +80,9 @@ def main():
         for file in file_assets:
             source = file['source']
             src = os.path.join(dir_in, source['path'])
-            if src.endswith('template.json'):
+            if src.endswith('nested.template.json'):
+                dst = os.path.abspath(os.path.join(REGIONAL_S3_ASSETS_PATH, file['_id']))
+            elif src.endswith('template.json'):
                 dst = os.path.abspath(os.path.join(GLOBAL_S3_ASSETS_PATH, file['_id'].replace('.json', '')))
             else:
                 dst = os.path.abspath(os.path.join(REGIONAL_S3_ASSETS_PATH, file['_id']))
