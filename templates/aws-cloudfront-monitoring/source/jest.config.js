447eafa9d8a3cfdaae3336fae5789b3a48c770b0
George Gu <minggu@amazon.com>
2022-01-18 15:53:15 +0800

    adding unit test for monitoring module

--- templates/aws-cloudfront-monitoring/source/jest.config.js
+++ templates/aws-cloudfront-monitoring/source/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   testEnvironment: 'node',
-  roots: ['<rootDir>/test'],
+  roots: ['<rootDir>/templates/test'],
   testMatch: ['**/*.test.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
