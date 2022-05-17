e3da2a040b389a39e30d00854aa39893a9eb395e
George Gu <minggu@amazon.com>
2022-05-17 15:19:26 +0800

    update appsync handler for ssl-for-saas

--- templates/console/source/lambda/ssl-for-saas/appsync_func/appsync_func.py
+++ templates/console/source/lambda/ssl-for-saas/appsync_func/appsync_func.py
@@ -5,51 +5,39 @@ import boto3
 import os
 import json
 import re
+from datetime import datetime
+
+from aws_lambda_powertools import Logger, Tracer
+from aws_lambda_powertools.logging import correlation_paths
+from aws_lambda_powertools.event_handler import AppSyncResolver
 
 LAMBDA_TASK_ROOT = os.environ.get('LAMBDA_TASK_ROOT')
-
-logger = logging.getLogger('boto3')
-logger.setLevel(logging.INFO)
 
 # add execution path
 os.environ['PATH'] = os.environ['PATH'] + ':' + os.environ['LAMBDA_TASK_ROOT']
 
+tracer = Tracer(service="ssl_for_saas_appsync_resolver")
+logger = Logger(service="ssl_for_saas_appsync_resolver")
 
+app = AppSyncResolver()
+
+@app.resolver(type_name="Mutation", field_name="certCreate")
+def certCreate(input):
+    logger.info(input)
+    data = {'createdAt': datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ'),
+            'status': "creating",
+            'updatedAt': datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')}
+    return data
+
+@app.resolver(type_name="Mutation", field_name="certImport")
+def certImport(input):
+    logger.info(input)
+    data = {'createdAt': datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ'),
+            'status': "creating",
+            'updatedAt': datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')}
+    return data
+
+@logger.inject_lambda_context(correlation_id_path=correlation_paths.APPSYNC_RESOLVER)
+@tracer.capture_lambda_handler
 def lambda_handler(event, context):
-    """
-
-    :param event:
-    :param context:
-    """
-
-    # "arguments": {
-    #     "input": {
-    #         "acm_op": "create/import",
-    #         "auto_creation": "true/false",
-    #         "dist_aggregate": "true/false",
-    #         "cnameList": [
-    #             {
-    #                 "domainName": "xx",
-    #                 "sanList": [
-    #                     "xx"
-    #                 ],
-    #                 "originsItemsDomainName": "xx"
-    #             },
-    #             {
-    #                 "domainName": "xx",
-    #                 "sanList": [
-    #                     "xx"
-    #                 ],
-    #                 "originsItemsDomainName": "xx"
-    #             }
-    #         ]
-    #     }
-    # }
-    logger.info("Received event: " + json.dumps(event))
-
-    # exact same code as acm_direct_op.py, TBD TODO: need to add
-
-    return {
-        'statusCode': 200,
-        'body': json.dumps('appsync_func.py: lambda_handler()')
-    }
+    return app.resolve(event, context)
