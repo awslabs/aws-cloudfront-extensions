05fcda3945266552253690bcdcc5fd22e104914b
George Gu <minggu@amazon.com>
2022-05-18 11:41:10 +0800

    update the appsync lambda handler

--- templates/console/source/lambda/ssl-for-saas/appsync_func/appsync_func.py
+++ templates/console/source/lambda/ssl-for-saas/appsync_func/appsync_func.py
@@ -18,24 +18,64 @@ os.environ['PATH'] = os.environ['PATH'] + ':' + os.environ['LAMBDA_TASK_ROOT']
 
 tracer = Tracer(service="ssl_for_saas_appsync_resolver")
 logger = Logger(service="ssl_for_saas_appsync_resolver")
+stepFunctionArn = os.environ.get('STEP_FUNCTION_ARN')
 
 app = AppSyncResolver()
 
+step_function = boto3.client('stepfunctions')
+
+
+# invoke step function with input
+def invoke_step_function(arn, input):
+    """[summary]
+    Args:
+        input ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    logger.info('start to invoke step function with input %s', input)
+    resp = step_function.start_execution(
+        stateMachineArn=arn,
+        input=json.dumps(input)
+    )
+
+    logger.info('step function invoked: %s', resp)
+
+    return resp
+
+
 @app.resolver(type_name="Mutation", field_name="certCreate")
-def certCreate(input):
+def cert_create(input):
     logger.info(input)
+
+    resp = invoke_step_function(stepFunctionArn, input)
+
+    # return {
+    #     'statusCode': 200,
+    #     'body': 'step function triggered with :' + str(resp['executionArn'])
+    # }
     data = {'createdAt': datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ'),
-            'status': "creating",
+            'status': str(resp),
             'updatedAt': datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')}
     return data
 
+
 @app.resolver(type_name="Mutation", field_name="certImport")
-def certImport(input):
+def cert_import(input):
     logger.info(input)
+
+    resp = invoke_step_function(stepFunctionArn, input)
+
+    # return {
+    #     'statusCode': 200,
+    #     'body': 'step function triggered with :' + str(resp['executionArn'])
+    # }
     data = {'createdAt': datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ'),
-            'status': "creating",
+            'status': str(resp),
             'updatedAt': datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')}
     return data
+
 
 @logger.inject_lambda_context(correlation_id_path=correlation_paths.APPSYNC_RESOLVER)
 @tracer.capture_lambda_handler
