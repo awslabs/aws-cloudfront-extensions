0533064970904cfe34b21f5b176c7a59b9d22ff1
George Gu <minggu@amazon.com>
2022-08-01 15:19:08 +0800

    fix: change ssl certification config when creating cloudfront dist

--- templates/console/source/lambda/ssl-for-saas/acm_cb_handler/acm_cb_handler.py
+++ templates/console/source/lambda/ssl-for-saas/acm_cb_handler/acm_cb_handler.py
@@ -452,5 +452,6 @@ def construct_cloudfront_config_with_dist_id(certificate_arn, default_cache_beha
     # config['DefaultCacheBehavior']['CachePolicyId'] = "658327ea-f89d-4fab-a63d-7e88639e58f6"
     config['ViewerCertificate'].pop('CloudFrontDefaultCertificate')
     config['ViewerCertificate']['ACMCertificateArn'] = certificate_arn
-    config['ViewerCertificate']['Certificate'] = certificate_arn
+    config['ViewerCertificate']['MinimumProtocolVersion'] = 'TLSv1.2_2021'
+    config['ViewerCertificate']['SSLSupportMethod'] = 'sni-only'
     return config
