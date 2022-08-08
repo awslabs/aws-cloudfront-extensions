da39e13ec7a66e9b5b8b9db901640d2e9c33f471
Ning <lvning@amazon.com>
2022-08-08 13:45:47 +0800

    doc: add more details

--- docs/en/faq.md
+++ docs/en/faq.md
@@ -7,7 +7,7 @@ Snapshot save the Cloudfront configuration file, you can get the same cloudfront
 Cloudfront configuration is complex, when user spent some time updating the cloudfront configuration, he/she don't want configuration been accidentally updated to wrong configuration. So we provide this Snapshot feature to let user create a snapshot so in the future user can compare the current configuration with snapshot or compare different between any two snapshots. 
 
 ### How long will Snapshot been saved?
-The snapshot will be saved in S3 bucket and will not automatically been deleted. The snapshot can only be deleted in Web Console or through [delete_snapshot api](api-reference-guide/snapshot/delete_snapshot.md) 
+The snapshot will be saved in S3 bucket and will not automatically been deleted. The snapshot can only be deleted in Web Console or through [delete_snapshot api](api-reference-guide/snapshot/delete_snapshot.md).
 
 ### How much will Snapshot cost?
-Cloudfront configuration file is json format and is around 3kb. Since the file is saved in S3, saving around 300000 snapshots will only cost $0.02 a month. 
+Cloudfront configuration file is in JSON format and is around 3KB. Refer to [Cost](cost.md) for more details.
---
+++ templates/console/deployment/build-s3-dist-deployer.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+# This assumes all of the OS-level configuration has been completed and git repo has already been cloned
+#
+# This script should be run from the repo's deployment directory
+# cd deployment
+# ./build-s3-dist.sh
+#
+# Check to see if input has been provided:
+# if [ "$1" ]; then
+#     echo "Usage: ./build-s3-dist.sh"
+#     exit 1
+# fi
+
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+set -euxo pipefail
+
+title() {
+    echo "------------------------------------------------------------------------------"
+    echo $*
+    echo "------------------------------------------------------------------------------"
+}
+
+run() {
+    >&2 echo "[run] $*"
+    $*
+}
+
+__dir="$(cd "$(dirname $0)";pwd)"
+SRC_PATH="${__dir}/../source"
+
+
+# Get reference for all important folders
+lambda_source_dir="$SRC_PATH/lambda"
+lib_dir="$SRC_PATH/lambda/common"
+lambda_build_dist_dir="$lib_dir/lambda-assets"
+
+echo "------------------------------------------------------------------------------"
+echo "[Init] Clean existed dist folders"
+echo "------------------------------------------------------------------------------"
+
+echo "rm -rf $lambda_build_dist_dir"
+rm -rf $lambda_build_dist_dir
+echo "mkdir -p $lambda_build_dist_dir"
+mkdir -p $lambda_build_dist_dir
+
+echo "find $lambda_source_dir -iname \"dist\" -type d -exec rm -r \"{}\" \; 2> /dev/null"
+find "$lambda_source_dir" -iname "dist" -type d -exec rm -r "{}" \; 2> /dev/null
+echo "find ../ -type f -name 'package-lock.json' -delete"
+find "$lambda_source_dir" -type f -name 'package-lock.json' -delete
+echo "find ../ -type f -name '.DS_Store' -delete"
+find "$lambda_source_dir" -type f -name '.DS_Store' -delete
+echo "find $lambda_source_dir -iname \"package\" -type d -exec rm -r \"{}\" \; 2> /dev/null"
+find "$lambda_source_dir" -iname "package" -type d -exec rm -r "{}" \; 2> /dev/null
+
+
+echo "------------------------------------------------------------------------------"
+echo "[Packing] Extensions repository deployer"
+echo "------------------------------------------------------------------------------"
+cd "$lambda_source_dir"/deployer || exit 1
+pip3 install -r requirements.txt --target ./package
+cd "$lambda_source_dir"/deployer/package || exit 1
+zip -q -r9 "$lambda_build_dist_dir"/deployer.zip .
+cd "$lambda_source_dir"/deployer || exit 1
+cp -r "$lambda_source_dir"/common .
+zip -g -r "$lambda_build_dist_dir"/deployer.zip deployer.py common
+
+
+echo "------------------------------------------------------------------------------"
+echo "[Packing] Extensions repository custom resource"
+echo "------------------------------------------------------------------------------"
+cd "$lambda_source_dir"/custom_resource || exit 1
+pip3 install -r requirements.txt --target ./package
+cd "$lambda_source_dir"/custom_resource/package || exit 1
+zip -q -r9 "$lambda_build_dist_dir"/custom_resource.zip .
+cd "$lambda_source_dir"/custom_resource || exit 1
+cp -r "$lambda_source_dir"/common .
+zip -g -r "$lambda_build_dist_dir"/custom_resource.zip custom_resource.py common
