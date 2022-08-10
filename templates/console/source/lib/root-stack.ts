231246be02530770184ed13eade70ed5a77ae25c
Yan Dai <cyanda@amazon.com>
2022-08-10 12:07:48 +0800

    [feat] fixed conflicts

--- templates/console/source/lib/root-stack.ts
+++ templates/console/source/lib/root-stack.ts
@@ -139,33 +139,33 @@ export class RootStack extends cdk.Stack {
             }
         );
 
-        // // SSL for SaaS stack
-        // new StepFunctionRpTsConstruct(this, "StepFunctionRpTsConstruct", {
-        //     /* If you don't specify 'env', this stack will be environment-agnostic.
-        //      * Account/Region-dependent features and context lookups will not work,
-        //      * but a single synthesized template can be deployed anywhere. */
-        //
-        //     /* Uncomment the next line to specialize this stack for the AWS Account
-        //      * and Region that are implied by the current CLI configuration. */
-        //     // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-        //
-        //     /* Uncomment the next line if you know exactly what Account and Region you
-        //      * want to deploy the stack to. */
-        //     // env: { account: '123456789012', region: 'us-east-1' },
-        //
-        //     /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-        //     synthesizer: props.synthesizer,
-        //     appsyncApi: commonConstruct.appsyncApi,
-        //     configVersionDDBTableName: configVersion.configVersionDDBTableName,
-        // });
-        //
-        // new ConsoleConstruct(this, "ConsoleConstruct", {
-        //     tags: {
-        //         app: "CloudFrontExtensionsConsole",
-        //     },
-        //     synthesizer: props.synthesizer,
-        //     appsyncApi: commonConstruct.appsyncApi,
-        // });
+        // SSL for SaaS stack
+        new StepFunctionRpTsConstruct(this, "StepFunctionRpTsConstruct", {
+            /* If you don't specify 'env', this stack will be environment-agnostic.
+             * Account/Region-dependent features and context lookups will not work,
+             * but a single synthesized template can be deployed anywhere. */
+
+            /* Uncomment the next line to specialize this stack for the AWS Account
+             * and Region that are implied by the current CLI configuration. */
+            // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+
+            /* Uncomment the next line if you know exactly what Account and Region you
+             * want to deploy the stack to. */
+            // env: { account: '123456789012', region: 'us-east-1' },
+
+            /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+            synthesizer: props.synthesizer,
+            appsyncApi: commonConstruct.appsyncApi,
+            configVersionDDBTableName: configVersion.configVersionDDBTableName,
+        });
+
+        new ConsoleConstruct(this, "ConsoleConstruct", {
+            tags: {
+                app: "CloudFrontExtensionsConsole",
+            },
+            synthesizer: props.synthesizer,
+            appsyncApi: commonConstruct.appsyncApi,
+        });
 
     }
 }
