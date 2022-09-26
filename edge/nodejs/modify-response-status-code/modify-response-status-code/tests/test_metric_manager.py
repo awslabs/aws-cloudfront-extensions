ae26baf73ed69d1ac6a481c0426208627d541617
Ning <lvning@amazon.com>
2022-09-26 09:35:33 +0800

    chore: remove unused extensions

--- edge/nodejs/modify-response-status-code/modify-response-status-code/tests/test_metric_manager.py
+++
@@ -1,2 +0,0 @@
-def test_query_metric_ddb():
-    assert False
--- edge/python/modify-host-origin-request-header/modify_host_origin_request_header/app.py
+++
@@ -1,6 +0,0 @@
-originDomain = 'ORIGIN_DOMAIN'
-
-def lambda_handler(event, context):
-    request = event['Records'][0]['cf']['request']
-    request['headers']['host'][0]['value'] = originDomain;
-    return request
