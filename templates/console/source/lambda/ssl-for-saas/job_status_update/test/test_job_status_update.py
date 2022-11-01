683dc9a2a481296e4fb1f934e2681b4e3c4b7221
George Gu <minggu@amazon.com>
2022-11-01 11:35:13 +0800

    chore: updating UT for job_status_update function

--- templates/console/source/lambda/ssl-for-saas/job_status_update/test/test_job_status_update.py
+++ templates/console/source/lambda/ssl-for-saas/job_status_update/test/test_job_status_update.py
@@ -203,4 +203,77 @@ def test_lambda_handler(monkeypatch):
     })
     lambda_handler(event, {})
 
+@mock_dynamodb
+@mock_acm
+def test_lambda_handler_with_failed_certification(monkeypatch):
+    monkeypatch.setenv("LAMBDA_TASK_ROOT", "Lambda root", prepend=False)
+
+    from job_status_update import lambda_handler
+    import job_status_update
+    acm = boto3.client('acm')
+    monkeypatch.setattr(acm, 'list_tags_for_certificate', lambda *args, **kwargs: {
+        'Tags': [
+            {
+                'Key': 'job_token',
+                'Value': '2f193ef0-5210-44d6-bb8f-559a8690461d'
+            }
+        ],
+        'CertificateSummaryList': [
+            {'CertificateArn': 'arn'}
+        ]
+    })
+    job_status_update.acm_client = acm
+    job_status_update.JOB_INFO_TABLE_NAME = 'ssl_for_saas_job_info_table'
+
+    ddb = boto3.resource(service_name="dynamodb", region_name="us-east-1")
+    ddb.create_table(
+        TableName='ssl_for_saas_job_info_table',
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'jobId',
+                'AttributeType': 'S'
+            },
+
+        ],
+        KeySchema=[
+            {
+                'AttributeName': 'jobId',
+                'KeyType': 'HASH'
+            },
+        ],
+        BillingMode='PROVISIONED',
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 10,
+            'WriteCapacityUnits': 10
+        },
+    )
+    ddb_table = ddb.Table('ssl_for_saas_job_info_table')
+    ddb_table.put_item(Item={
+        'jobId': '2f193ef0-5210-44d6-bb8f-559a8690461d',
+        'cert_total_number': 1,
+        'certValidationStageStatus': 'FAIL',
+        'dcv_validation_msg': 'CNAME value need to add into DNS hostzone to finish DCV',
+        'job_input': '{\"cert_total_number\": 1, \"auto_creation\": \"true\",\"certValidationStageStatus\": \"INPROGRESS\",\"dcv_validation_msg\":\"CNAME value need to add into DNS hostzone to finish DCV\"}'
+    })
+
+    monkeypatch.setattr(job_status_update, 'update_job_field', lambda *args, **kwargs: {})
+    monkeypatch.setattr(acm, 'list_certificates', lambda *args, **kwargs: {
+        'CertificateSummaryList': [
+            {'CertificateArn': ''}
+        ]
+    })
+    monkeypatch.setattr(acm, 'list_tags_for_certificate', lambda *args, **kwargs: {
+        'Tags': [{'Key': 'job_token', 'Value': '2f193ef0-5210-44d6-bb8f-559a8690461d'}]
+    })
+
+    lambda_handler(event, {})
+
+    ddb_table.put_item(Item={
+        'jobId': '2f193ef0-5210-44d6-bb8f-559a8690461d',
+        'cert_total_number': 2,
+        'certValidationStageStatus': 'FAIL',
+        'job_input': '{\"cert_total_number\": 1, \"auto_creation\": \"false\",\"certValidationStageStatus\": \"SUCCESS\"}'
+    })
+    lambda_handler(event, {})
+
 
