f9bdc5534768e9bd768660401b37e326ff466d5c
christofile <liang333916@126.com>
2022-11-12 21:37:37 +0800

    add cache-control-with-regex-path codes

---
+++ edge/nodejs/cache-control-with-regex-path/cache-control-with-regex-path/tests/unit/test-handler.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const app = require('../../app.js');
+const chai = require('chai');
+const expect = chai.expect;
+var event, context;
+
+var event = {
+    "Records": [
+      {
+        "cf": {
+          "config": {
+            "distributionDomainName": "d111111abcdef8.cloudfront.net",
+            "distributionId": "EDFDVBD6EXAMPLE",
+            "eventType": "viewer-request",
+            "requestId": "4TyzHTaYWb1GX1qTfsHhEqV6HUDd_BzoBZnwfnvQc_1oF26ClkoUSEQ=="
+          },
+          "request": {
+            "clientIp": "203.0.113.178",
+            "headers": {
+              "host": [
+                {
+                  "key": "Host",
+                  "value": "d111111abcdef8.cloudfront.net"
+                }
+              ],
+              "user-agent": [
+                {
+                  "key": "User-Agent",
+                  "value": "curl/7.66.0"
+                }
+              ],
+              "cache-control": [
+                {
+                  "key": "cache-control",
+                  "value": "no-cache"
+                }
+              ],
+              "accept": [
+                {
+                  "key": "accept",
+                  "value": "*/*"
+                }
+              ]
+            },
+            "method": "GET",
+            "querystring": "",
+            "uri": "/"
+          }
+        }
+      }
+    ]
+  };
+var context;
+
+var callback = function(request, response) {
+    return response
+}
+
+describe('Tests index', function () {
+    it('verifies successful response', async () => {
+        const result = await app.handler(event, context, callback)
+        expect(result).to.undefined;
+
+        // expect(result.status).to.equal(200);
+        // expect(result.body).to.be.an('string');
+        // let response = JSON.parse(result.body);
+        // expect(response).to.be.an('object');
+
+    });
+});
