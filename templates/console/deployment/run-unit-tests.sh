3a9f91f1c7dafcf59db615ce1d2ea7c9682ff8a1
Ning <lvning@amazon.com>
2022-12-29 12:21:25 +0800

    chore: add ut script

--- templates/console/deployment/run-unit-tests.sh
+++ templates/console/deployment/run-unit-tests.sh
@@ -7,13 +7,43 @@
 # ./run-unit-tests.sh
 #
 
-template_dir="$PWD"
-source_dir="$template_dir/../source"
+# template_dir="$PWD"
+# source_dir="$template_dir/../source"
+
+# echo "------------------------------------------------------------------------------"
+# echo "[Test] Console testing"
+# echo "------------------------------------------------------------------------------"
+# echo 'pip3 install -r ../source/tests/lambda-test/testing_requirements.txt'
+# pip3 install -r ../source/tests/lambda-test/testing_requirements.txt
+# echo 'pytest -s ../source/tests/lambda-test/'
+# pytest -s ../source/tests/lambda-test/
 
 echo "------------------------------------------------------------------------------"
-echo "[Test] Console testing"
+echo "[Test] Monitoring test coverage"
 echo "------------------------------------------------------------------------------"
-echo 'pip3 install -r ../source/tests/lambda-test/testing_requirements.txt'
-pip3 install -r ../source/tests/lambda-test/testing_requirements.txt
-echo 'pytest -s ../source/tests/lambda-test/'
-pytest -s ../source/tests/lambda-test/
+
+cd ../source/lambda/monitoring/non_realtime
+
+for d in */ ; do
+    echo "$d"
+    pushd $d
+    cp ../shared_lib/python/metric_helper.py .
+    python3 -m pytest --cov --cov-report=term-missing --cov-report "xml:./non_$(basename $(pwd)).coverage.xml"
+    rm -rf metric_helper.py
+    cp non_$(basename $(pwd)).coverage.xml ../../../../tests/coverage-reports/
+    rm -rf non_$(basename $(pwd)).coverage.xml
+    cd ..
+done
+
+cd ../realtime
+
+for d in */ ; do
+    echo "$d"
+    cd $d
+    cp ../shared_lib/python/metric_helper.py .
+    python3 -m pytest --cov --cov-report=term-missing --cov-report "xml:./$(basename $(pwd)).coverage.xml"
+    rm -rf metric_helper.py
+    cp $(basename $(pwd)).coverage.xml ../../../../tests/coverage-reports/
+    rm -rf $(basename $(pwd)).coverage.xml
+    cd ..
+done
