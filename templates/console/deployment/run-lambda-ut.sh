3b12f8e26097d37ad7135ce9ca006ac294422d40
Yan Dai <cyanda@amazon.com>
2023-03-07 12:40:14 +0800

    pipeline developing

--- templates/console/deployment/run-lambda-ut.sh
+++ templates/console/deployment/run-lambda-ut.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-EXCLUDE=("ssl_for_saas" "venv" "python-env")
+EXCLUDE=("ssl_for_saas" "venv" "python-env" "monitoring")
 cd ../source || exit
+
+COVERAGE_REPORTS=${PWD}/all-coverages
+mkdir -p $COVERAGE_REPORTS
 
 RED='\033[0;31m'
 NC='\033[0m' # No Color
@@ -22,7 +25,7 @@ function process_requirements() {
          if [[ "$1" == requirements*txt ]]; then
            printf "${GREEN} found requirements file:${NC} ${PWD}/$1 \n"
            if [ -s $1 ]; then
-             pip install -r "$1"
+             pip install -r "$1" -i https://pypi.tuna.tsinghua.edu.cn/simple
            fi
            echo
          fi
@@ -32,24 +35,23 @@ function process_requirements() {
 
 function process_test() {
   if [[ " ${EXCLUDE[*]} " =~ $1 ]]; then
-    pwd
-    echo " ${EXCLUDE[*]} "
     echo "exclude file: $1"
   else
     if [ -d "$1" ]; then
       cd "$1" || exit
-      local test=0
+
       for d in * ; do
-        if [ -f $d ]; then
-          if [[ "$d" =~ test_.*py ]]; then
-            printf "${GREEN}$d${NC}\n"
-            test=1
+        if [ -d $d ]; then
+          if [ $d == test ]; then
+            printf "${PWD}: ${RED}$d${NC}\n"
+            python3 -m pytest --cov --cov-report=term-missing --cov-report "xml:./$(basename "${PWD}").coverage.xml"
+            mv *.coverage.xml $COVERAGE_REPORTS
+          else
+            printf "${YELLOW}$d${NC}\n"
+            process_test "$d"
           fi
-        else
-          process_test "$d"
         fi
       done
-      [ $test -eq 1 ] && python3 -m pytest --cov --cov-report=term-missing --cov-report "xml:./non_$(basename $(pwd)).coverage.xml"
       cd .. || return
     fi
   fi
